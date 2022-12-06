138d6d283135d679d5785663034ddb62f66afbd5
Juan Verón <juan_juve97@hotmail.com>
2022-12-06 02:10:46 -0300

    added folder es6 whit files

---
+++ ES6/17.Write Concise Declarative Functions with ES6.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+const bicycle = {
+    gear: 48,
+    setGear(newGear){
+      return {newGear}
+    }
+    
+  };
+  // Only change code above this line
+  bicycle.setGear(3);
+  console.log(bicycle.gear);
